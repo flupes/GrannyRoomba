6971f19e7a92c1411b9e58fd56babe819e67e934
Lorenzo Flueckiger <lorenzo@flupes.org>
2013-08-28 17:42:02 -0700

    Transformed into a Service with the launching Activity doing nothing else but launch and create a Notification allowing to Stop the servive

--- code/org.flupes.ljf.grannyroomba.controller/gen/org/flupes/ljf/grannyroomba/controller/BuildConfig.java
+++ code/org.flupes.ljf.grannyroomba.controller/gen/org/flupes/ljf/grannyroomba/controller/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package org.flupes.grannyroomba.robotcontroller;
+package org.flupes.ljf.grannyroomba.controller;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- code/org.flupes.ljf.grannyroomba.controller/src/org/flupes/ljf/grannyroomba/controller/GrannyRoombaService.java
+++ code/org.flupes.ljf.grannyroomba.controller/src/org/flupes/ljf/grannyroomba/controller/GrannyRoombaService.java
@@ -3,7 +3,11 @@ package org.flupes.ljf.grannyroomba.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.os.IBinder;
 
@@ -18,7 +22,7 @@ public class GrannyRoombaService extends Service {
 
 	protected Server m_servoService;
 	protected IServo m_servoImpl;
-	
+
 	@Override
 	public void onCreate() {
 		s_logger.debug("GrannyRoombaService.onCreate");
@@ -35,6 +39,29 @@ public class GrannyRoombaService extends Service {
 	@Override
 	public void onStart(Intent intent, int startid) {
 		s_logger.debug("GrannyRoombaService.onStart");
+		
+		NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		if (intent != null && intent.getAction() != null && intent.getAction().equals("stop")) {
+			// User clicked the notification. Need to stop the service.
+			nm.cancel(0);
+			stopSelf();
+		}
+		else {
+			// Service starting. Create a notification.
+			Intent stopMsg = new Intent("stop", null, this, this.getClass());
+			PendingIntent notifMsg = PendingIntent.getService(this,
+					0, stopMsg, PendingIntent.FLAG_UPDATE_CURRENT);
+
+			Notification notification = new Notification.Builder(this)
+			.setContentTitle("GrannyRoomba service running...")
+			.setContentText("Click Stop to terminate the GrannyRoomba service")
+			.setSmallIcon(R.drawable.ic_launcher)
+			.addAction(R.drawable.ic_launcher, "Stop", notifMsg)
+			.setProgress(0, 0, true)
+			.build();
+			
+			nm.notify(0, notification);
+		}
 		m_servoService.start();
 	}
 
