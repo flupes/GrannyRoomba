db6c3ba29b1a7397e16dd56570fcf2ff2d5f9ae5
Lorenzo Flueckiger <lorenzo@flupes.org>
2013-08-30 09:03:24 -0700

    Adjust classpath

--- tests/JeroMqAndroid/gen/org/flupes/grannyroomba/test/jmqandroid/helloserver/R.java
+++ tests/JeroMqAndroid/gen/org/flupes/grannyroomba/test/jmqandroid/helloserver/R.java
@@ -17,8 +17,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f060000;
-        public static final int activity_vertical_margin=0x7f060001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -35,12 +35,12 @@ public final class R {
         public static final int jzmq_server=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040001;
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040002;
-        public static final int toggleOff=0x7f040005;
-        public static final int toggleOn=0x7f040004;
-        public static final int togglebutton=0x7f040003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int toggleOff=0x7f060005;
+        public static final int toggleOn=0x7f060004;
+        public static final int togglebutton=0x7f060003;
     }
     public static final class style {
         /** 
@@ -104,7 +104,7 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f040000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
  Application theme. 
@@ -112,6 +112,6 @@ public final class R {
  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f040001;
     }
 }
