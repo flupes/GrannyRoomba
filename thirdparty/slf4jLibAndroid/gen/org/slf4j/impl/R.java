22724d939b516f9d91faa3e90622fe090fd8ea2a
Lorenzo Flueckiger <lorenzo@flupes.org>
2013-08-30 06:58:16 -0700

    Remove the "app" types of properties from the projects since these are libraries only

--- thirdparty/slf4jLibAndroid/gen/org/slf4j/impl/R.java
+++ thirdparty/slf4jLibAndroid/gen/org/slf4j/impl/R.java
@@ -13,9 +13,6 @@ public final class R {
     public static final class drawable {
         public static int ic_launcher=0x7f020000;
     }
-    public static final class string {
-        public static int app_name=0x7f030000;
-    }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
@@ -38,10 +35,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static int AppBaseTheme=0x7f040000;
+        public static int AppBaseTheme=0x7f030000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static int AppTheme=0x7f040001;
+        public static int AppTheme=0x7f030001;
     }
 }
