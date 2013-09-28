5ba4b9cabe6553e89f3ab2651e39cfcb0fdca732
Lorenzo Flueckiger <lorenzo@flupes.org>
2013-09-27 17:40:27 -0700

    Added a test for the Server

---
+++ code/org.flupes.ljf.grannyroomba.pctests/src/org/flupes/ljf/grannyroomba/pctests/TestServer.java
@@ -0,0 +1,56 @@
+package org.flupes.ljf.grannyroomba.pctests;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.Appender;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.TTCCLayout;
+import org.flupes.ljf.grannyroomba.net.Server;
+
+public class TestServer {
+
+	private static Logger s_logger = Logger.getLogger("grannyroomba");
+	private Server m_server;
+	
+	class SimpleServer extends Server {
+
+		private int m_counter;
+		
+		public SimpleServer(int port) {
+			super(port, 1000);
+			s_logger.info("Server initialized");
+		}
+
+		@Override
+		public void loop() throws InterruptedException {
+			m_counter++;
+			s_logger.info("SimpleServer loop " + m_counter);
+		}
+		
+	}
+	
+	public TestServer() {
+		s_logger.info("building server");
+		m_server = new SimpleServer(8888);
+		s_logger.info("starting server");
+		m_server.start();
+		s_logger.info("sleeping for 6s");
+		try {
+			Thread.sleep(6000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		s_logger.info("stoping the server");
+		m_server.cancel();
+		s_logger.info("terminate");
+	}
+
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger("grannyroomba");
+		logger.setLevel(Level.TRACE);
+		Appender appender = new ConsoleAppender(new TTCCLayout(), ConsoleAppender.SYSTEM_OUT);
+		logger.addAppender(appender);
+		new TestServer();
+	}
+
+}
