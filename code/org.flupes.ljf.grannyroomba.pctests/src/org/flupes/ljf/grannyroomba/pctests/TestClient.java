c16a3d5f57f355ceb3264cec7d98cce70bfdb5f3
Lorenzo Flueckiger <lorenzo@flupes.org>
2013-10-09 21:10:32 -0700

    Added a test client. This tester for ZmqClient can be configured on the command line.

---
+++ code/org.flupes.ljf.grannyroomba.pctests/src/org/flupes/ljf/grannyroomba/pctests/TestClient.java
@@ -0,0 +1,77 @@
+package org.flupes.ljf.grannyroomba.pctests;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.Appender;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.TTCCLayout;
+import org.flupes.ljf.grannyroomba.net.ZmqClient;
+import org.zeromq.ZMQException;
+
+
+public class TestClient {
+
+	private static Logger s_logger = Logger.getLogger("grannyroomba");
+	private SimpleClient m_client;
+
+	private int m_received = 0;
+
+	private final String request = "Hello";
+	
+	class SimpleClient extends ZmqClient {
+
+		public SimpleClient(String server, int port) {
+			super(server, port);
+			s_logger.info("Client initialized");
+		}
+
+		public void test() {
+			byte[] reply = m_client.reqrep(request.getBytes());
+			if ( reply != null ) {
+				m_received++;
+			}
+		}
+
+	}
+
+	public TestClient() {
+		
+		String hostname = "localhost";
+		String hostProp = System.getProperties().getProperty("host");
+		if ( hostProp != null ) {
+			hostname = hostProp;
+		}
+		int port = Integer.getInteger("port", 8888);
+		int requests = Integer.getInteger("requests", 10);
+		
+		s_logger.info("building client");
+		m_client = new SimpleClient(hostname, port);
+		m_client.connect();
+
+		try {
+			long start = System.nanoTime();
+			for (int i=0; i<requests; i++) {
+				m_client.test();
+			}
+			long stop = System.nanoTime();
+			s_logger.info("Received " + m_received + " valid responses to "
+					+ requests + " requests in "
+					+ (stop-start)/1E6+ "ms");
+		} catch ( ZMQException e) {
+			s_logger.warn("Error in REQ/REP: " + e.getErrorCode());
+		}
+
+		
+		m_client.disconnect();
+	}
+
+
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger("grannyroomba");
+		logger.setLevel(Level.TRACE);
+		Appender appender = new ConsoleAppender(new TTCCLayout(), ConsoleAppender.SYSTEM_OUT);
+		logger.addAppender(appender);
+		new TestClient();
+	}
+
+}
