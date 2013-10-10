1719f799da27d0f527cb95171cc24777f87826a6
Lorenzo Flueckiger <lorenzo@flupes.org>
2013-10-09 21:08:54 -0700

    Two improvements: 1) Fix a bug to terminate correctly: need to disconnect the socket first 2) Set timeout on the send / recv to avoid being blocked if the server dies 3) Try to encapsulate the req/rep pattern in a single call

--- code/org.flupes.ljf.grannyroomba.net/src/org/flupes/ljf/grannyroomba/net/ZmqClient.java
+++ code/org.flupes.ljf.grannyroomba.net/src/org/flupes/ljf/grannyroomba/net/ZmqClient.java
@@ -3,6 +3,7 @@ package org.flupes.ljf.grannyroomba.net;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zeromq.ZMQ;
+import org.zeromq.ZMQException;
 
 public class ZmqClient {
 
@@ -31,16 +32,30 @@ public class ZmqClient {
 			m_context = ZMQ.context(1);
 			m_socket = m_context.socket(ZMQ.REQ);
 			m_socket.connect (m_url);
+			m_socket.setSendTimeOut(500);
+			m_socket.setReceiveTimeOut(1500);
 			m_connected = true;
 			s_logger.info("Client connected to ["+m_url+"]");
 		}
 	}
 	
 	public synchronized void disconnect() {
+		m_socket.disconnect(m_url);
 		m_socket.close();
 		m_context.term();
 		m_connected = false;
 		s_logger.info("Client of [" + m_url + "] disconnected.");
 	}
 	
+	protected byte[] reqrep(byte[] buffer) throws ZMQException {
+		boolean req = m_socket.send(buffer);
+		if ( ! req ) {
+			throw new ZMQException(m_socket.base().errno());
+		}
+		byte[] rep = m_socket.recv(0);
+		if ( null == rep ) {
+			throw new ZMQException(m_socket.base().errno());
+		}
+		return rep;
+	}
 }
