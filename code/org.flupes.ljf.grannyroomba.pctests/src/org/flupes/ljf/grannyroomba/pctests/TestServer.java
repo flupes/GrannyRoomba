660a45f544dc0be2779a1b4f4dc6c4685eadc26b
Lorenzo Flueckiger <lorenzo@flupes.org>
2013-10-09 21:11:33 -0700

    Improved the test Server: This test for ZmqServer stays alive until a sigint is received, and it displays some status.

--- code/org.flupes.ljf.grannyroomba.pctests/src/org/flupes/ljf/grannyroomba/pctests/TestServer.java
+++ code/org.flupes.ljf.grannyroomba.pctests/src/org/flupes/ljf/grannyroomba/pctests/TestServer.java
@@ -12,37 +12,71 @@ public class TestServer {
 	private static Logger s_logger = Logger.getLogger("grannyroomba");
 	private ZmqServer m_server;
 	
+	private int m_counter;
+	private volatile boolean up = true;
+	
 	class SimpleServer extends ZmqServer {
 
-		private int m_counter;
+		private final String response = "World";
 		
-		public SimpleServer(int port) {
-			super(port, 1000);
+		public SimpleServer(int port, int sleepMs) {
+			super(port, sleepMs);
 			s_logger.info("Server initialized");
 		}
 
 		@Override
 		public void loop() throws InterruptedException {
+			byte[] data = m_socket.recv();
+			if ( data != null ) {
+				m_socket.send(response.getBytes());
+			}
 			m_counter++;
-			s_logger.info("SimpleServer loop " + m_counter);
 		}
 		
 	}
 	
 	public TestServer() {
+		
 		s_logger.info("building server");
-		m_server = new SimpleServer(8888);
+		int port = Integer.getInteger("port", 8888);
+		int delay = Integer.getInteger("delay", 1000);
+		
+		m_server = new SimpleServer(port, delay);
+		
 		s_logger.info("starting server");
 		m_server.start();
-		s_logger.info("sleeping for 4s");
-		try {
-			Thread.sleep(4000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
+			public void run() {
+				up = false;
+				s_logger.warn("Process received INT signal");
+				s_logger.info("Processed " + m_counter + " requests");
+				s_logger.info("stoping the server");
+				m_server.cancel();
+				s_logger.info("terminate");
+			}
+		});
+
+		int loop = 0;
+		int lastCounter = m_counter;
+		while ( up ) {
+			try {
+				loop++;
+				System.out.print(".");
+				if ( m_counter > lastCounter ) {
+					System.out.print(" "+m_counter+" ");
+					lastCounter = m_counter;
+				}
+				if ( 30 == loop ) {
+					System.out.println();
+				}
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
-		s_logger.info("stoping the server");
-		m_server.cancel();
-		s_logger.info("terminate");
+		
 	}
 
 	public static void main(String[] args) {
