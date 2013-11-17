12a1abf88e57658bc30be16f19146e453c9177dc
Lorenzo Flueckiger <lorenzo@flupes.org>
2013-11-17 10:34:26 -0800

    measure time even when not in debug mode

--- code/org.flupes.ljf.grannyroomba.hw/src/org/flupes/ljf/grannyroomba/hw/Chronometer.java
+++ code/org.flupes.ljf.grannyroomba.hw/src/org/flupes/ljf/grannyroomba/hw/Chronometer.java
@@ -10,37 +10,33 @@ public class Chronometer {
 
 	private long m_start;
 	private long m_stop;
-	private long m_duration;
+	private int m_duration;
 	private String m_name;
 
 	public Chronometer(String name) {
 		m_name = name;
 	}
-	
+
 	public void start() { 
-		if ( m_checkTiming ) {
-			m_start = System.nanoTime();
-		}
+		m_start = System.nanoTime();
 	}
-	
+
 	public void stop() {
-		if ( m_checkTiming ) {
-			m_stop = System.nanoTime();
-			m_duration = (m_stop-m_start)/1000000;
-		}
+		m_stop = System.nanoTime();
+		m_duration = (int)((m_stop-m_start)/1000000);
 	}
-	
-	public long duration() {
+
+	public int duration() {
 		return m_duration;
 	}
-	
+
 	public void show() {
 		if ( m_checkTiming ) {
 			s_logger.warn("Timer [" + m_name + "] -> " + m_duration + "ms");
 		}
 	}
-	
-	public void show(long enough) {
+
+	public void show(int enough) {
 		if ( m_checkTiming && m_duration>enough ) {
 			s_logger.warn("Timer [" + m_name + "] -> " + m_duration + "ms");
 		}
