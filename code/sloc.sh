933bb607ffac52a2fa06a314a408006724e5579b
Lorenzo Flueckiger <lorenzo@flupes.org>
2013-11-17 21:07:54 -0800

    sloc counter script added

---
+++ code/sloc.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+dirs="org.flupes.ljf.grannyroomba org.flupes.ljf.grannyroomba.controller org.flupes.ljf.grannyroomba.hw org.flupes.ljf.grannyroomba.net org.flupes.ljf.grannyroomba.pctests"
+sloccount $dirs
